bf4be1568d7a1a7bbaf545e352895153c3f67647
TRMT-Yuka <momo144.art@gmail.com>
2022-01-31 14:57:57 +0900

    add model_sh and token-classification-legacy

---
+++ model_sh/gpu1/run_ner_x50.sh
@@ -0,0 +1,8 @@
+python3 token-classification-legacy/run_ner_gpu1.py \
+  --model_name_or_path bert-base-uncased \
+  --data_dir data_augment/SimpleDA_BI/x50 \
+  --output_dir model_output/SimpleDA_BI/x50 \
+  --overwrite_output_dir true\
+  --do_train true \
+  --do_eval true \
+  --do_predict true
