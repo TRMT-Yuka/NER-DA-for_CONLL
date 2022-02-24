18f6321e90b910a687b9eb4f282ad4c405452368
TRMT-Yuka <momo144.art@gmail.com>
2022-02-24 15:16:57 +0900

    Reset and copy from gpu

--- model_sh/gpu0/run_ner_original.sh
+++ model_sh/gpu0/run_ner_original.sh
@@ -1,7 +1,7 @@
 python3 token-classification-legacy/run_ner_gpu0.py \
   --model_name_or_path bert-base-uncased \
-  --data_dir data_augment/SyntacticDA_BI/x2 \
-  --output_dir model_output/SyntacticDA_BI/x2 \
+  --data_dir data_original \
+  --output_dir model_output/original \
   --overwrite_output_dir true\
   --do_train true \
   --do_eval true \
